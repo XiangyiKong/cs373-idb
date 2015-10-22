16f4eb0d4d27820c1c4f188d8f992906fecec57b
Wei-Chieh Chen <wc6892@wireless-10-147-226-30.public.utexas.edu>
2015-10-22 16:35:12 -0500

    views.py fixed

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, jsonify, request
 from marvel_api import (get_list_of_characters, get_single_character, get_list_of_comics, get_single_comic, get_list_of_events,
-	get_single_event, get_list_of_series, get_single_series)
+	get_single_event, get_list_of_series, get_single_series, get_single_creator, get_list_of_creators)
 import re
 
 @app.route('/')
